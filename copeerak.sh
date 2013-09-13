2c4f9077dafdb9df6447625338dd6bd3514bbfdf
mr.Shu <mr@shu.io>
2013-09-13 16:56:42 +0200

    better detection

--- copeerak.sh
+++ copeerak.sh
@@ -2,6 +2,11 @@
 
 while [ true ]; do
         OUT=$(timeout 3 cdda2wav -J -g /dev/sr0 >& /dev/stdout)
-        [[ -n `echo $OUT | grep load` ]] && echo "no load" 
-        sleep 3
+        if [[ -n `echo $OUT | grep load` ]]; then
+                echo "no load" 
+        elif [[ -n `echo $OUT | grep 'This disk has no audio'` ]]; then
+                echo "data disc" 
+        else
+                echo "media CD"
+        fi
 done
